2f2d5520c866cc7dd3920d673ba9ce94c68ed0a5
User <funikov.1997@mail.ru>
2023-02-04 16:36:18 +0300

    Работа с методами передачи данных

--- datas/script.php
+++ datas/script.php
@@ -1,6 +1,16 @@
 <?php
 
-echo $_GET['name'].'<br>';
-echo $_GET['age'].'<br>';
-echo $_GET['color'].'<br>';
-echo $_GET['select'].'<br>';
+$lables = [
+    'Имя пользователя',
+    'Возраст пользователя',
+];
+
+foreach ($_POST as $key => $value) {
+    echo "Поле с атрибутом $key имеет значение $value<br>";
+}
+
+
+//echo $_GET['name'].'<br>';
+//echo $_GET['age'].'<br>';
+//echo $_GET['color'].'<br>';
+//echo $_GET['select'].'<br>';
