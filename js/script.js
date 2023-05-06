a7de38ed958fdcb1d4df8f562c793f8e1794fefc
User <funikov.1997@mail.ru>
2023-05-06 18:02:55 +0300

    Основы JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
 
 function alert_message(text) {
-    alert(text)
+    let variable = 'text'
+    let variable2 = 'text'
+}
+
+function get_element(id) {
+    document.getElementById(id).value;
+}
+
+function get_summ() {
+    let first = document.getElementById('first').value
+    let second = document.getElementById('second').value
+    let sum = parseInt(first) + parseInt(second);
+    document.getElementById('result').innerHTML = "Сумма чисел равна: " + sum;
 }
